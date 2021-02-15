97d1f23a513534aff6e0097accdc028d3d4427fb
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-02-15 14:06:46 +0100

    Add project files.

---
+++ K_Eszter_2021.02.15_struktura/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K_Eszter_2021._02._15_struktura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
